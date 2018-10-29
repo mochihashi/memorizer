29803eb6751a42837825715835d966334db40e2f
mochihashi <h.mochihashi@biztech-fusion.com>
2018-10-29 13:18:04 +0900

    .

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 cd $(dirname $0)
 rm -f memorizer.zip
 cd ../
-zip memorizer.zip memorizer/*.* -x memorizer/*.sh
+zip memorizer.zip memorizer/index.html
+zip memorizer.zip memorizer/favicon.ico
 zip -r memorizer.zip memorizer/lib/
 mv memorizer.zip memorizer/
