50efbc5eb2d64235ba0160c3338a168649881a76
mochihashi <h.mochihashi@biztech-fusion.com>
2018-10-29 10:47:28 +0900

    manifest

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 cd $(dirname $0)
 rm -f memorizer.zip
 cd ../
-zip memorizer.zip memorizer/index.html
-zip memorizer.zip memorizer/favicon.ico
+zip memorizer.zip memorizer/*.* -x memorizer/*.sh
 zip -r memorizer.zip memorizer/lib/
 mv memorizer.zip memorizer/
